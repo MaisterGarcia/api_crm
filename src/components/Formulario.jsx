5b5fe91b1b0d0abaa0c34ff1def2f04bb9ff6fe4
MaisterGarcia <jorge_08emmanuel@hotmail.com>
2022-07-18 14:16:58 -0500

    first commit

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,167 @@
+import { Formik, Form, Field } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import Alerta from "./Alerta";
+import Spinner from "../components/Spinner";
+
+const Formulario = ({ cliente, cargando }) => {
+  const navigate = useNavigate();
+  const nuevoClienteSchema = Yup.object().shape({
+    nombre: Yup.string()
+      .min(3, "El nombre es muy corto")
+      .max(20, "El nombre es muy largo")
+      .required("El Nombre del Cliente es Obligatorio"),
+    empresa: Yup.string().required("El nombre de la empresa es obligatorio"),
+    email: Yup.string()
+      .email("Email no valido")
+      .required("El email es obligatorio"),
+    telefono: Yup.number()
+      .positive("Número no válido")
+      .integer("Número no válido")
+      .typeError("El número no es válido"),
+  });
+  const handleSubmit = async (valores) => {
+    try {
+      let respuesta;
+      if (cliente.id) {
+        const url = `http://localhost:4000/clientes/${cliente.id}`;
+        respuesta = await fetch(url, {
+          method: "PUT",
+          body: JSON.stringify(valores),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+      } else {
+        const url = "http://localhost:4000/clientes";
+        respuesta = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(valores),
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+      }
+      await respuesta.json();
+      navigate("/clientes");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return cargando ? (
+    <Spinner />
+  ) : (
+    <div className="bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto">
+      <h1 className="text-gray-600 font-bold text-xl uppercase text-center">
+        {cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}
+      </h1>
+      <Formik
+        initialValues={{
+          nombre: cliente?.nombre ?? "",
+          empresa: cliente?.empresa ?? "",
+          email: cliente?.email ?? "",
+          telefono: cliente?.telefono ?? "",
+          notas: cliente?.notas ?? "",
+        }}
+        onSubmit={async (valores, { resetForm }) => {
+          await handleSubmit(valores);
+          resetForm();
+        }}
+        validationSchema={nuevoClienteSchema}
+        enableReinitialize={true}
+      >
+        {({ errors, touched }) => {
+          return (
+            <Form className="mt-10">
+              <div className="mb-4">
+                <label htmlFor="nombre" className="text-gray-800">
+                  Nombre:
+                </label>
+                <Field
+                  id="nombre"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Nombre del cliente"
+                  name="nombre"
+                />
+                {errors.nombre && touched.nombre ? (
+                  <Alerta>{errors.nombre}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="emrpesa" className="text-gray-800">
+                  Empresa:
+                </label>
+                <Field
+                  id="empresa"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Empresa del cliente"
+                  name="empresa"
+                />
+                {errors.empresa && touched.empresa ? (
+                  <Alerta>{errors.empresa}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="email" className="text-gray-800">
+                  E-mail:
+                </label>
+                <Field
+                  id="email"
+                  type="email"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Email del cliente"
+                  name="email"
+                />
+                {errors.email && touched.email ? (
+                  <Alerta>{errors.email}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="telefono" className="text-gray-800">
+                  Teléfono:
+                </label>
+                <Field
+                  id="telefono"
+                  type="tel"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Teléfono del cliente"
+                  name="telefono"
+                />
+                {errors.telefono && touched.telefono ? (
+                  <Alerta>{errors.telefono}</Alerta>
+                ) : null}
+              </div>
+              <div className="mb-4">
+                <label htmlFor="notas" className="text-gray-800">
+                  Notas:
+                </label>
+                <Field
+                  as="textarea"
+                  id="notas"
+                  type="text"
+                  className="mt-2 block w-full p-3 bg-gray-50"
+                  placeholder="Notas del cliente"
+                  name="notas"
+                />
+              </div>
+              <input
+                type="submit"
+                value={cliente?.nombre ? "Editar Cliente" : "Agregar Cliente"}
+                className="mt-5 w-full bg-blue-800 p-3 text-white uppercase font-bold text-lg"
+              />
+            </Form>
+          );
+        }}
+      </Formik>
+    </div>
+  );
+};
+Formulario.defaultProps = {
+  cliente: {},
+};
+
+export default Formulario;
---
+++ src/paginas/Inicio.jsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import Cliente from "../components/Cliente";
+
+const Inicio = () => {
+  const [clientes, setClientes] = useState([]);
+  useEffect(() => {
+    const obtenerClientes = async () => {
+      try {
+        const url = "http://localhost:4000/clientes";
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        setClientes(resultado);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    obtenerClientes();
+  }, []);
+  const handleEliminar = async (id) => {
+    try {
+      const confirmar = confirm("¿Desea eliminar este cliente?");
+      if (confirmar) {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url, {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        await respuesta.json();
+      }
+      const arrayClientes = clientes.filter((cliente) => cliente.id !== id);
+      setClientes(arrayClientes);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+      <table className="w-full mt-5 table-auto shadow bg-white">
+        <thead className="bg-blue-800 text-white">
+          <tr>
+            <th className="p-2">Nombre</th>
+            <th className="p-2">Contacto</th>
+            <th className="p-2">Empresa</th>
+            <th className="p-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientes.map((cliente) => (
+            <Cliente
+              key={cliente.id}
+              cliente={cliente}
+              handleEliminar={handleEliminar}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default Inicio;
